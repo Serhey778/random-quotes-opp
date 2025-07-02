f2e7dfb96acc2960a18583bbf96d8201de122368
Sviatlana <sviatlanamukha05@gmail.com>
2025-07-02 12:17:15 +0300

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: '800001',
+    text: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    id: '800002',
+    text: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    id: '800003',
+    text: 'You are never too old to set another goal or to dream a new dream',
+    author: 'C.S. Lewis',
+  },
+  {
+    id: '800004',
+    text: 'Act as if what you do makes a difference. It does',
+    author: 'William James',
+  },
+  {
+    id: '800005',
+    text: 'The best way to predict the future is to create it',
+    author: 'Peter Drucker',
+  },
+  {
+    id: '800006',
+    text: 'It does not matter how slowly you go as long as you do not stop',
+    author: 'Confucius',
+  },
+  {
+    id: '800007',
+    text: 'Dream big and dare to fail',
+    author: 'Norman Vaughan',
+  },
+  {
+    id: '800008',
+    text: 'Success usually comes to those who are too busy to be looking for it',
+    author: 'Henry David Thoreau',
+  },
+  {
+    id: '800009',
+    text: 'What we fear doing most is usually what we most need to do',
+    author: 'Tim Ferriss',
+  },
+  {
+    id: '800010',
+    text: 'The best revenge is massive success',
+    author: 'Frank Sinatra',
+  },
+];
+
+module.exports = quotes;
